5b37f74fde3afdf508505afc8554de1e6513b746
Sonar Systems <frahaanhussain@Frahaans-MacBook-Pro.local>
2015-12-05 23:46:23 +0000

    Another code folder

---
+++ Code2/index.php
@@ -0,0 +1,24 @@
+<?php require_once( "Elements/HEADER.php" ); ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-8" id="OutputContainer">
+          <canvas width="500px" height="500px"></canvas>
+        </div>
+
+        <div class="col-xs-4" id="InputContainer">
+          <h2>Input</h2>
+
+            <div class="form-group">
+              <label for="comment">Code:</label>
+              <textarea class="form-control" rows="5" id="CodeTextArea" name="CodeTextArea"></textarea>
+            </div>
+
+            <button type="submit" id="RunCode" name="RunCode" class="btn btn-primary btn-block">Run The Code :D</button>
+        </div>
+      </div>
+    </div>
+
+    <div id="TestColour" style="display:hidden;"></div>
+
+<?php require_once( "Elements/FOOTER.php" ); ?>
